570f9acc0ff2bea169528595bf6e715bb55b7d4b
yoonh12 <yoonhogeon06@gmail.com>
2023-05-26 19:16:19 +0900

    third

--- src/pages/Rental.js
+++ src/pages/Rental.js
@@ -3,16 +3,16 @@ import { useState } from "react";
 import Footer from "./components/Footer";
 
 function Rental() {
-  const [userName, setUserName] = useState("");
-  const [Passwd, setPasswd] = useState("");
+  const [userId, setUserId] = useState("");
+  // const [Passwd, setPasswd] = useState("");
 
-  const onUsrId = (e) => {
-    setUserName(e.target.value);
+  const onUserId = (e) => {
+    setUserId(e.target.value);
   };
 
-  const onPassWd = (e) => {
-    setPasswd(e.target.value);
-  };
+  // const onPassWd = (e) => {
+  //   setPasswd(e.target.value);
+  // };
 
   return (
     <div className="main">
@@ -20,19 +20,19 @@ function Rental() {
         <h1 className="formTitle">우산 대여</h1>
         <form className="loginForm">
           <input
-            name="name"
+            name="userId"
             type="text"
-            onChange={onUsrId}
-            value={userName}
-            placeholder="아이디"
+            onChange={onUserId}
+            value={userId}
+            placeholder="학번"
           />
-          <input
+          {/* <input
             name="password"
             type="password"
             onChange={onPassWd}
             value={Passwd}
             placeholder="비밀번호"
-          />
+          /> */}
           <button className="formBtnRental" type="submit">
             지금 대여하기!
           </button>
