edf9c3382e2e15eb73ae3c9462a82da5c7ce4844
yoonh12 <yoonhogeon06@gmail.com>
2023-06-04 18:03:02 +0900

    font & success page & new design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 
 function Button(props) {
   return <button className="btn">{props.btnText}</button>;
@@ -9,17 +9,18 @@ function Home() {
   return (
     <div className="main">
       <div className="container">
-        <h1 className="title">우산 대여 서비스</h1>
+        <h1 className="title">안동고등학교<br/>우산 대여 서비스</h1>
         <div className="btnCon">
           <Link to="/rental">
-            <Button btnText="대여" />
+            <Button btnText="대여하기" />
           </Link>
           <Link to="/return">
-            <Button btnText="반납" />
+            <Button btnText="반납하기" />
           </Link>
         </div>
+        <p className="license">LICENSE @AHS @NEOFLUX<span className="ifu">이용안내</span></p>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/pages/Success.js
+++ src/pages/Success.js
@@ -2,27 +2,36 @@ import "../return.css";
 import { useLocation } from "react-router-dom";
 import Footer from "./components/Footer";
 
-let newDate = new Date();
-let date = newDate.getDate();
-let month = newDate.getMonth() + 1;
-let year = newDate.getFullYear();
+let curDate = new Date();
+let date = curDate.getDate();
+let month = curDate.getMonth() + 1;
+let year = curDate.getFullYear();
+
+let reDate = curDate.getDate() + 5;
+console.log(curDate.toLocaleDateString("ko-KR"));
 
 function Success() {
   const { state } = useLocation();
   if (state == null) {
     window.history.back();
   }
+
   const curDate = `${year}/${month < 10 ? `0${month}` : `${month}`}/${
     date < 10 ? `0${date}` : `${date}`
+  }`;
+
+  const returnDate = `${year}/${month < 10 ? `0${month}` : `${month}`}/${
+    date < 10 ? `0${reDate}` : `${reDate}`
   }`;
 
   return (
     <div className="main">
       <div className="container">
         <h1 className="title">
-          {state == "rental" ? "대여가" : "반납이"} 완료되었습니다.<br/>
-          {curDate}
+          {state === "rental" ? "대여가" : "반납이"} 완료되었습니다.
         </h1>
+        <p>대여 날자: {curDate}</p>
+        <p>반납 기한: {returnDate}</p>
       </div>
       <Footer />
     </div>
@@ -30,3 +39,4 @@ function Success() {
 }
 
 export default Success;
+// seungmin chun jae
