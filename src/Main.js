d77bc67ca3b9da429840e9ca4072ccc9814b785c
yoonh12 <yoonhogeon06@gmail.com>
2023-06-10 20:49:27 +0900

    Rename Rental.js

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
-import Rental from './pages/Rental';
+import Rental from './pages/FormRental';
 import Return from './pages/Return';
 import ScanRental from "./pages/ScanRental";
 import Success from "./pages/Success";
