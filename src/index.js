f1455b00a13aea47b5e30224a00b29d434ab2a1e
Yoonh <dev.yoonh@itm-kr.com>
2023-07-05 10:18:18 +0900

    merge index.js and Main.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import App from "./Main";
+import Home from "./pages/Home";
+import Loading from "./pages/Loading";
+import Rental from "./pages/FormRental";
+import Return from "./pages/Return";
+import ScanRental from "./pages/ScanRental";
+import Success from "./pages/Success";
+import HandleError from "./pages/Error";
+import DirectProcess from "./DirectProcess";
+import Info from "./pages/Info";
+import Delay from "./pages/Delay";
+
 import "./styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/loading" element={<Loading />} />
+        <Route path="/rental" element={<Rental />} />
+        <Route path="/return" element={<Return />} />
+        <Route path="/scan" element={<ScanRental />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/fail" element={<HandleError />} />
+        <Route path="/delay" element={<Delay />} />
+        <Route path="/ifu" element={<Info />} />
+        <Route path="/quick" element={<DirectProcess />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 root.render(<App />);
