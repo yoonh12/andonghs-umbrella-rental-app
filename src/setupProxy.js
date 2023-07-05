00122e55ad289932ee04d58a28f84366ae479964
Yoonh <dev.yoonh@itm-kr.com>
2023-07-05 10:01:31 +0900

    middleware proxy 추가

---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = (app) => {
+	app.use(
+		createProxyMiddleware('/api', {
+			target: process.env.REACT_APP_API_URL, 
+			changeOrigin: true,
+		})
+	);
+};
